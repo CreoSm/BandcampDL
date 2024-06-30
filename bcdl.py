06537c39435b901dbd0769f706892d348d00fe17
Vezxe <ayyitscreo@gmail.com>
2024-06-30 11:16:34 +0000

    Show progess bar while downloading songs.

--- bcdl.py
+++ bcdl.py
@@ -10,6 +10,15 @@ def PrintUsage():
     Also supports multiple urls!!!\n
         bcdl.py [bandcamp url 1] [bandcamp url 2]""")
 
+out=sys.stdout
+pbsize = 20 # Progress Bar Size
+def progressb(j,m):
+    perc = 100/m*j
+    x = x = int(pbsize*j/m)
+
+    
+    print("[{}{}] [ {}% ]".format(">"*x, " "*(pbsize-x), int(perc)),  end='\r', file=out, flush=True)
+
 def DL(url):
     SongName = url.split("/")[4]
     print(f"Beginning Download For {SongName}")
@@ -21,8 +30,14 @@ def DL(url):
     content = content[start_of_stream_url:]
     DownloadURL = content[:content.find("&quot")]
     print("(3/3) Downloading")
-    dl = requests.get(DownloadURL, allow_redirects=True)
-    open(SongName+".mp3", 'wb').write(dl.content)
+    dl = requests.get(DownloadURL, allow_redirects=True, stream=True)
+    file_size = int(dl.headers.get('Content-Length', None))
+    with open(SongName+".mp3", 'wb') as file:
+        for i, chunk in enumerate(dl.iter_content(chunk_size=1024)):
+            progressb((i+1)*1024,file_size)
+            file.write(chunk)
+    print("\n")
+
 
 def CheckURLType(url):
     if url.split("/")[3] == "track":
